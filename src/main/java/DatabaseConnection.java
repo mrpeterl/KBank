4b570f1ffb87906a10222d15e745208905504843
Christopher O'Hagan <christophero@kainos.com>
2017-10-05 12:07:28 +0100

    Database connection first version

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,2 +1,33 @@
+import java.sql.*;
+import java.util.Scanner;
+
 public class DatabaseConnection {
+    public static void main(String password, String command) {
+        Scanner test = new Scanner(System.in);
+        System.out.println("Please enter password");
+        password = test.nextLine();
+        String passwordTest = password;
+        System.out.println("Please enter sql command");
+        command = test.nextLine();
+
+        try {
+            Class driver = Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:://localhost/demo", "root", "password");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(command);
+            while(resultSet.next()) {
+                String name = resultSet.getString("name");
+                System.out.println(name);
+
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
