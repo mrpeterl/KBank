e2d65c68f155afdf188ffd093b1c9912ffcbaea5
Michael Dunwoody <michaeld@kainos.com>
2017-10-05 11:40:17 +0100

    Added Java main class and squashed a couple annoying bugs

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        menu();
+    }
+    
+    public static void menu(){
+        System.out.println("Welcome to KBank!");
+
+    }
+}
+
